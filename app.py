77fee6483911d950bdc8f89b65718fc3e48e39b2
raftouch <ntimorshina@gmail.com>
2025-03-27 23:34:13 +0100

    cursor, create table, close connection

--- app.py
+++ app.py
@@ -2,3 +2,23 @@ import sqlite3
 
 # connection = sqlite3.connect(':memory:')
 connection = sqlite3.connect('customer.db')
+
+connection = sqlite3.connect('customer.db')
+
+c = connection.cursor()
+
+c.execute(""""CREATE TABLE cutomers (
+          first_name TEXT,
+          last_name TEXT,
+          email TEXT
+    )""")
+
+# NULL
+# INTEGER
+# REAL
+# TEXT
+# BLOB
+
+connection.commit()
+
+connection.close()
