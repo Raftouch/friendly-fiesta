4da3beefc249bdbbaea8f32689319fc3493e8c83
raftouch <ntimorshina@gmail.com>
2025-03-27 23:39:50 +0100

    add one record to table

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ connection = sqlite3.connect('customer.db')
 
 c = connection.cursor()
 
-c.execute(""""CREATE TABLE cutomers (
+c.execute(""""CREATE TABLE customers (
           first_name TEXT,
           last_name TEXT,
           email TEXT
@@ -18,6 +18,8 @@ c.execute(""""CREATE TABLE cutomers (
 # REAL
 # TEXT
 # BLOB
+
+c.execute("INSERT INTO customers VALUES ('Bob', 'Bryan', bobbryan@test.com')")
 
 connection.commit()
 
