bd226f22479024c14a1e86c8f56d6aca6871b77f
raftouch <ntimorshina@gmail.com>
2025-03-27 23:45:46 +0100

    add multiple records into db

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ connection = sqlite3.connect('customer.db')
 
 c = connection.cursor()
 
-c.execute(""""CREATE TABLE customers (
-          first_name TEXT,
-          last_name TEXT,
-          email TEXT
-    )""")
+# c.execute("""CREATE TABLE customers (
+#           first_name TEXT,
+#           last_name TEXT,
+#           email TEXT
+#     )""")
 
 # NULL
 # INTEGER
@@ -19,8 +19,18 @@ c.execute(""""CREATE TABLE customers (
 # TEXT
 # BLOB
 
-c.execute("INSERT INTO customers VALUES ('Bob', 'Bryan', bobbryan@test.com')")
+customers = [
+    ('Bob', 'Bryan', 'bobbryan@test.com'),
+    ('Alice', 'Paris', 'aliceparis@test.com'),
+    ('Anna', 'Banana', 'annabanana@test.com')
+]
+
+# c.execute("INSERT INTO customers VALUES ('Bob', 'Bryan', bobbryan@test.com')")
+
+c.executemany("INSERT INTO customers (first_name, last_name, email) VALUES (?, ?, ?)", customers)
 
 connection.commit()
 
 connection.close()
+
+print("Records inserted successfully 🙂")
