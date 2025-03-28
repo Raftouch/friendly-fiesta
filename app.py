cf8f2bba5419db6d8a9b2ea194608ac839b4ed61
raftouch <ntimorshina@gmail.com>
2025-03-28 18:52:57 +0100

    update, format, order by

--- app.py
+++ app.py
@@ -27,10 +27,33 @@ customers = [
 
 # c.execute("INSERT INTO customers VALUES ('Bob', 'Bryan', bobbryan@test.com')")
 
-c.executemany("INSERT INTO customers (first_name, last_name, email) VALUES (?, ?, ?)", customers)
+# c.executemany("INSERT INTO customers (first_name, last_name, email) VALUES (?, ?, ?)", customers)
+
+# c.execute("SELECT * FROM customers")
+# c.execute("SELECT rowid, * FROM customers")
+c.execute("SELECT rowid, * FROM customers ORDER BY rowid DESC")
+# c.execute("SELECT * FROM customers WHERE last_name = 'Paris'")
+# c.execute("SELECT * FROM customers WHERE last_name LIKE '%an%'")
+
+# c.execute("""UPDATE customers SET first_name = 'Bob'
+#           WHERE rowid = 1
+#         """)
+
+
+# fetchOne = c.fetchone()
+# print(fetchOne[0])
+# print("****************")
+# c.fetchmany(3)
+items = c.fetchall()
+print(items)
+
+for item in items:
+    print(item)
+    # print(item[0] + " " + item[1] + " \t " + item[2])
+
 
 connection.commit()
 
 connection.close()
 
-print("Records inserted successfully 🙂")
+print("Command executed successfully 🙂")
